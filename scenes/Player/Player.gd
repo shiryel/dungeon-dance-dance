a5cc0af3e4ec5415aed5cdca7acaaddb020fd3b3
RoxoFoxo <gab.apfprado@gmail.com>
2020-08-23 05:25:00 -0300

    Added skull and bones to spawn when the Player dies and made it so that you can't shoot fireballs while dead

--- scenes/Player/Player.gd
+++ scenes/Player/Player.gd
@@ -8,6 +8,7 @@ signal dead
 
 var target = Vector2()
 var velocity = Vector2()
+var dead = false
 
 func _physics_process(_delta): # movement
 	velocity = (target - position).normalized() * speed
@@ -21,7 +22,7 @@ func _process(_delta):
 	if Input.is_action_pressed('ui_mouse_left'): # sets target for movement
 		target = get_global_mouse_position()
 
-	if Input.is_action_just_pressed('ui_mouse_right'): # fireballs
+	if Input.is_action_just_pressed('ui_mouse_right') and not dead: # fireballs
 		var fireball = Fireball.instance()
 		fireball.position = position
 		fireball.linear_velocity = get_global_mouse_position() - position
@@ -32,5 +33,7 @@ func _on_Area2D_body_entered(_body):
 
 func _on_Hud_dead():
 	$AnimatedSprite.hide()
+	$SkullAndBones.visible = true
 	emit_signal("dead")
+	dead = true
 	speed = 0 # the player can't move while dead (meio gambiarra)
